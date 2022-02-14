8e088e4b23bfe2abf58db9bff71deba3f2d69c5c
Muraleedharn <rtrmuralee@gmail.com>
2022-02-14 11:34:27 +0530

    test cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('name') inputName: any;
   title = 'todoList';
+  hideElement=true;
+  count:any;
+  list:any[]=[];
+  getList(item:any){
+    this.list.push({id:this.list.length,task:item});
+    console.log(this.list);
+    this.inputName.nativeElement.value = '';
+  }
+  constructor(){
+    this.count=(this.list.length==0)? "true" : "false";
+  }
+  removeTask(id:number){
+    this.list=this.list.filter(item=>item.id!==id)
+  }
 }
